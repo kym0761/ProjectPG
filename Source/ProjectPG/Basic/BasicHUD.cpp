0cc24250d41d290e06c3a68d346c2cdaf5e4364f
KimYongMin <kym0761@naver.com>
2020-09-14 11:52:31 +0900

    UI Assets

---
+++ Source/ProjectPG/Basic/BasicHUD.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasicHUD.h"
+#include "Engine/Canvas.h"
+#include "Engine/StreamableManager.h"
+#include "Engine/Texture2D.h"
+void ABasicHUD::DrawHUD()
+{
+	Super::DrawHUD();
+
+	//Draw2DLine(100, 100, 200, 200, FColor::Red);
+	//DrawLine(300, 300, 400, 400, FLinearColor::Green, 10.0f);
+	if (Crosshair)
+	{
+		DrawTextureSimple(Crosshair, (Canvas->SizeX / 2) - (Crosshair->GetSizeX() / 2), (Canvas->SizeY / 2) - (Crosshair->GetSizeY() / 2));
+	}
+	//DrawTextureSimple(,);
+}
+
+void ABasicHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FStreamableManager loader;
+
+
+	T_Crosshair.SetPath(TEXT("Texture2D'/Game/UI/crosshair.crosshair'"));
+
+	//Crosshair = loader.LoadSynchronous<UTexture2D>(T_Crosshair);
+
+
+	loader.RequestAsyncLoad(T_Crosshair, FStreamableDelegate::CreateUObject(this, &ABasicHUD::LoadHUDTexture));
+
+	
+}
+
+void ABasicHUD::LoadHUDTexture()
+{
+	Crosshair = Cast<UTexture2D>(T_Crosshair.TryLoad());
+}
---
+++ Source/ProjectPG/Basic/BasicHUD.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "BasicHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTPG_API ABasicHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void DrawHUD() override;
+
+	virtual void BeginPlay() override;
+
+	class UTexture2D* Crosshair;
+
+	UFUNCTION()
+	void LoadHUDTexture();
+
+	FSoftObjectPath T_Crosshair;
+
+};
